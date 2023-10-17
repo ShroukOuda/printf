c1f9ae42cd6c2b37a7bf25a5ac28da46d6f6824a
ShroukOuda <s36568961@gmail.com>
2023-10-17 09:17:27 -0700

    commit

---
+++ _printf.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * _printf - check the code.
+ * @format: it's a format
+ * Return: Always 0.
+ */
+int _printf(const char *format, ...)
+{
+	unsigned int n, m = 0, cnt = 0;
+	char *s;
+	va_list prf;
+
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+	va_start(prf, format);
+	for (n = 0; format[n] != '\0'; n++)
+	{
+		if (format[n] != '%')
+		{
+			_putchar(format[n]);
+		}
+		if (format[n] == '%')
+		{
+			if (format[n + 1] == 'c')
+			{
+				_putchar(va_arg(prf, int));
+				n++;
+			}
+			else if (format[n + 1] == '%')
+			{
+				_putchar('%');
+				n++;
+			}
+			else if (format[n + 1] == 's')
+			{
+				s = va_arg(prf, char *);
+			while (s[m])
+			{
+			_putchar(s[m]);
+		m++;
+			}
+			}
+		}
+	cnt++;
+	}
+return (cnt);
+}
